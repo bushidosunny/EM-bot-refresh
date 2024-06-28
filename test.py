f8f08a653cdf3aa7cc88afe8862511acca5712d3
bushidosunny <32914548+bushidosunny@users.noreply.github.com>
2024-06-28 14:17:41 -0700

    update archive

--- test.py
+++
@@ -1,9 +0,0 @@
-
-
-from datetime import datetime
-import pytz
-
-timezone = pytz.timezone("America/Los_Angeles")
-current_time = datetime.now(timezone)
-formatted_time = current_time.strftime("%H:%M:%S")
-print(formatted_time)
