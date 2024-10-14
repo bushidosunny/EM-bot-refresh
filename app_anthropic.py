c370e561305e8b115d24fa92258e4cd5d879a76d
bushidosunny <bushidosunny@gmail.com>
2024-10-14 09:27:40 -0700

    tryy st rerun in mobile.py

--- app_anthropic.py
+++ app_anthropic.py
@@ -3246,11 +3246,9 @@ def is_mobile():
 
 def mobile_user():
     text = render_mobile()
-    loading_message = "EMMA is creating follow up Questions and Examinations..."
     if text is not None:
         process_user_question(text, st.session_state.specialist, mobile=True)
-        with st.spinner(loading_message):
-            display_mobile_ddx_follow_up()
+        display_mobile_ddx_follow_up()
 
 ############################################# Main Function #############################################
 
--- mobile.py
+++ mobile.py
@@ -93,6 +93,7 @@ def handle_record_audio_mobile(audio):
             else:
                 prompt = transcript.replace("Speaker 0:", "").strip()
                 return prompt
+        st.rerun()
     else:
         processing_message.empty()
 
